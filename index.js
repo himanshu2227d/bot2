174e58b193e3a94e621bde0f13e403a828cecbb7
himanshu2227d <himanshuka7983@gmail.com>
2026-09-21 18:14:38 +0530

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const app = express();
 const config = {
     host: 'domainsmp.indernos.in', 
     port: 25565,            
-    username: 'BATMAN',    
+    username: 'Naturo',    
     version: '1.16.5',      
     password: 'password123'
 };
